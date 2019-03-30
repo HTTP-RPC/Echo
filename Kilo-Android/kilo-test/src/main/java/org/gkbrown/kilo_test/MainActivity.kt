e8c4021b50060f6bde2a107ec179990e182a65ca
Greg Brown <gk_brown@icloud.com>
2019-03-30 07:32:02 -0400

    Stub out Android test project.

---
+++ Kilo-Android/kilo-test/src/main/java/org/gkbrown/kilo_test/MainActivity.kt
@@ -0,0 +1,12 @@
+package org.gkbrown.kilo_test
+
+import android.app.Activity
+import android.os.Bundle
+
+class MainActivity : Activity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
